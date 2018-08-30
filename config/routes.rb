95b18bc28246788f83212fcc756c8a9b7fb9524f
oula nakhle <oulanakhle@gmail.com>
2018-08-30 16:52:28 +0200

    routes to learning

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'pages/dashboard'
+  # get 'goals/:goal_id/task/edit'
   resources :subjects, only: [:index, :show]
-  resources :goals, only: [:create, :show, :new,:destroy] do
-    resources :tasks, only: [:create]
+  resources :goals, only: [:create, :show, :new,:destroy,:index] do
+    resources :tasks, only: [:create,:new,:show,:index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

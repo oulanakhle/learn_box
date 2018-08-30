e094114973a80968ea5132f7053ab1f83645510f
jcllobet <jcllobet@gmail.com>
2018-08-31 00:22:51 +0200

    redirected the pages controller to the right dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get 'pages/dashboard'
+  get '/dashboard', to: 'pages#dashboard'
 
   # get 'goals/:goal_id/task/edit'
   resources :subjects, only: [:index, :show]

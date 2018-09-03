83583d5353efae9023927e09ea7d99294e943dcd
jcllobet <jcllobet@gmail.com>
2018-09-03 16:25:40 +0200

    changes on tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,7 +20,7 @@ class TasksController < ApplicationController
 private
 
 def task_params
-  params.require(:task).permit(:name, :due_date, :hours)
+  params.require(:task).permit(:name, :hours)
 end
 
 end

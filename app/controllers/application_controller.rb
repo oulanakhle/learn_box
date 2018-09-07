0285f66e73a4bf69394634ddc15006d196257b26
jcllobet <jcllobet@gmail.com>
2018-09-07 05:53:54 +0200

    uploaded meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if:  :devise_controller?
 
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
   protected
 
     def configure_permitted_parameters
